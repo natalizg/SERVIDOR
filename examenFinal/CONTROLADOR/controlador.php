1d341ad09c15aa022568ce3dbcb90b63c4655e34
Natalia <natalizg1999@gmail.com>
2024-02-06 12:55:18 +0100

    examen update

--- examenFinal/CONTROLADOR/controlador.php
+++ examenFinal/CONTROLADOR/controlador.php
@@ -10,12 +10,29 @@
     //Menú inicio que redirige dependiendo de si eres cliente o empleado:
     if(isset($_POST['opcion'])){
         $opcion = $_POST['opcion'];
-        if($opcion = 'cliente'){
+        if($opcion == 'cliente'){
             $data['body'] = 'VISTA/vistaRegistroCliente.php';
         }else{
             $data['body'] = 'VISTA/vistaInicioEmpleado.php';
         }
     }
+
+    //Registro Cliente:
+    if(isset($_POST['registrar'])){
+        $correo = $_POST['correo'];
+        $contrasena = $_POST['contrasena'];
+        $conexion = crear_conexion(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        $conexion->query();
+
+    }
+
+    //Inicio sesión cliente:
+    
+    if(isset($_POST['inicioSesion'])){
+        $data['body'] = 'VISTA/vistaInicioCliente.php'
+    }
+
+
     require('VISTA/LAYOUT/layout.php');
     
 
--- examenFinal/VISTA/vistaRegistroCliente.php
+++ examenFinal/VISTA/vistaRegistroCliente.php
@@ -1,1 +1,10 @@
 <h1>REGISTRO CLIENTE</h1>
+<form action="index.php" method="post">
+    <label for="correo">Correo Electrónico</label>
+    <input type="text" name="correo">
+    <label for="contrasena">Contraseña</label>
+    <input type="text" name="contrasena">
+    <button type="submit" name="registrar" value="registrar">Regístrate</button>
+    <p>¿Ya tienes cuenta?</p>
+    <input type="submit" name="iniciarSesion" value="Iniciar sesión">
+</form>
