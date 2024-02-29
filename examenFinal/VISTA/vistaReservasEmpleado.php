daad6d527b6381497bd8082657c4ebff71a34307
Natalia <natalizg1999@gmail.com>
2024-02-29 16:47:16 +0100

    update

--- examenFinal/VISTA/vistaReservasEmpleado.php
+++ examenFinal/VISTA/vistaReservasEmpleado.php
@@ -1,45 +1,52 @@
-<h1>RESERVAS CLIENTES</h1>
 
-<?php
-    if(isset($reservaEliminada)){
-        ?>
-            <h3 style="color:blue">Reserva Eliminada</h3>
+
+<div class="contenedor">
+    <div class="title">
+        <h2>RESERVAS CLIENTES</h2>
+    </div>
+    <div class="interior">
         <?php
-    }
-?>
+            if(isset($reservaEliminada)){
+                ?>
+                    <h3 style="color:blue">Reserva Eliminada</h3>
+                <?php
+            }
+        ?>
 
-<form action="index.php" method="post">
-    <label for="fecha">Seleccionar Fecha:</label>
-    <input type="date" id="fecha" name="fecha" value="<?php echo date('Y-m-d'); ?>">
-    <button type="submit" name="filtrarReservas">Filtrar</button>
-</form>
+        <form action="index.php" method="post">
+            <label for="fecha">Seleccionar Fecha:</label>
+            <input type="date" id="fecha" name="fecha" value="<?php echo date('Y-m-d'); ?>">
+            <button type="submit" name="filtrarReservas">Filtrar</button>
+        </form>
 
-<form action="index.php" method="post">
+        <form action="index.php" method="post">
 
-    <table>
-        <thead>
-            <tr>
-                <th>Correo</th>
-                <th>Fecha</th>
-                <th>Hora</th>
-                <th>Mesa</th>
-                <th>Descripción</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($reservas as $reserva): ?>
-                <tr>
-                    <th><?php echo $reserva['CORREO_CLIENTE']?></th>
-                    <th> <?php echo $reserva['FECHA']?></th>
-                    <th> <?php echo $reserva['HORA']?></th>
-                    <th> <?php echo $reserva['MESA']?></th>
-                    <th> <?php echo $reserva['DESCRIPCION']?></th>
-                    <th><button type="submit" name="eliminarReservaEmpleado" value="<?php echo $reserva['FECHA'] . '_' . $reserva['HORA'] . '_' . $reserva['MESA']; ?>">Cancelar</button></th>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-        
-    </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Correo</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Mesa</th>
+                        <th>Descripción</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($reservas as $reserva): ?>
+                        <tr>
+                            <th><?php echo $reserva['CORREO_CLIENTE']?></th>
+                            <th> <?php echo $reserva['FECHA']?></th>
+                            <th> <?php echo $reserva['HORA']?></th>
+                            <th> <?php echo $reserva['MESA']?></th>
+                            <th> <?php echo $reserva['DESCRIPCION']?></th>
+                            <th><button type="submit" name="eliminarReservaEmpleado" value="<?php echo $reserva['FECHA'] . '_' . $reserva['HORA'] . '_' . $reserva['MESA']; ?>">Cancelar</button></th>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+                
+            </table>
 
-</form>
+        </form>
+    </div>
+</div>
